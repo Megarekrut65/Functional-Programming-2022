f18a1e1b4f36cdcc0a485b68617f3b4f8e5f3256
Megarekrut65 <55782081+Megarekrut65@users.noreply.github.com>
2022-10-31 18:01:00 +0200

    Added adding articles to database

--- Lab1/app/Main.hs
+++ Lab1/app/Main.hs
@@ -1,4 +1,31 @@
 module Main where
 
+import Control.Applicative
+import Database.SQLite.Simple
+import Database.SQLite.Simple.FromRow
+import Data.Time
+
+data Article = Article  { articleId::Int
+                        , title::String
+                        , description::String
+                        }
+instance Show Article where
+   show article = mconcat [ show $ articleId article
+                       , ".)  "
+                       , title article
+                       , " "
+                       , description article
+                       , "\n"]
+withConn :: String -> (Connection -> IO ()) -> IO ()
+withConn dbName action = do
+   conn <- open dbName
+   action conn
+   close conn
+addArticle :: Int -> String -> String -> IO ()
+addArticle articleId title description = withConn "FacultyNewspaper.db" $
+                                    \conn -> do
+                                      execute conn
+                                        "INSERT INTO article (id, title, description) VALUES (?, ?, ?);"
+                                        (articleId, title, description)
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do addArticle 3 "Hello" "World"
